0d12a94b47c767bd85bf9c1b6725056b444530ae
Isaac Mendoza <Isaac@MacBook-Pro-de-Isaac.local>
2019-11-23 21:17:25 -0600

    Add map

--- onc/src/components/App.jsx
+++ onc/src/components/App.jsx
@@ -3,12 +3,13 @@ import React from 'react'
 import Home from '../views/Home'
 import NavBar from './NavBar'
 import Banner from './Banner'
+import MapSection from "./MapSection";
 
 function App () {
     return(
         <div>
             <NavBar />
-            <Banner />
+            <MapSection/>
             <Home />
         </div>
     )
---
+++ onc/src/components/MapSection.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function MapSection() {
+  return(
+  <div className= "container d-flex">
+    <div className="col-md-6">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/e/ed/Mapa_Electoral_Federal_de_Ciudad_de_M%C3%A9xico.png" alt=""/>
+    </div>
+    <div className= "col-md-6  .align-middle p-5" >
+      <h5 className= "text-center">Lista de alcaldias</h5>
+      
+    </div>
+  </div>
+  )
+}
+
+export default MapSection
