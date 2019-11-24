a57405866119029f1926ff3d30bb54bc475274ce
Isaac Mendoza <Isaac@MacBook-Pro-de-Isaac.local>
2019-11-24 06:36:21 -0600

    Add Imgs and Banner

--- onc/src/components/App.jsx
+++ onc/src/components/App.jsx
@@ -10,6 +10,7 @@ function App () {
     return(
         <div>
             <NavBar />
+            <Banner/>
             <MapSection/>
             <Home />
         </div>
--- onc/src/components/Banner.jsx
+++ onc/src/components/Banner.jsx
@@ -1,35 +1,30 @@
 import React from "react";
-
-
+import uno from "../assets/img/1.png"
+import dos from "../assets/img/2.png"
+import tres from "../assets/img/3.png"
 function Banner() {
   return(
-    <div className="bd-example">
-      <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
-        <ol className="carousel-indicators">
-          <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
-          <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
-          <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
-        </ol>
-        <div className="carousel-inner">
-          <div className="carousel-item active">
-            <img src="
-            " className="d-block w-100" alt="..."/>
-            <div className="carousel-caption d-none d-md-block">
-              <h5>First slide label</h5>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </div>
-          </div>
-                  </div>
-        <a className="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
-          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span className="sr-only">Previous</span>
-        </a>
-        <a className="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
-          <span className="carousel-control-next-icon" aria-hidden="true"></span>
-          <span clasNames="sr-only">Next</span>
-        </a>
+      <div id="carouselExampleIndicators" class="carousel-inner w-100% h-200" role="listbox">
+    <ol className="carousel-indicators">
+      <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    </ol>
+    <div className="carousel-inner">
+      <div className="carousel-item active">
+        <img className="d-block w-100" src={uno} alt="First slide"/>
+      </div>
+      <div class="carousel-item">
+        <img className="d-block w-100" src={dos} alt="Second slide"/>
+      </div>
+      <div className="carousel-item">
+        <img className="d-block w-100" src={tres} alt="Third slide"/>
       </div>
     </div>
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev" onclick="$('#myCarousel').carousel('prev')">‹</a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next" onclick="$('#myCarousel').carousel('next')">›</a>
+  </div>
+
   )
 }
 
