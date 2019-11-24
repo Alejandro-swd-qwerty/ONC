074533ef30b3a13a78f4a10cda7aa4e6d74cc2cb
Isaac Mendoza <Isaac@MacBook-Pro-de-Isaac.local>
2019-11-23 20:11:23 -0600

    Add banner config

--- onc/src/components/App.jsx
+++ onc/src/components/App.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 
 import Home from '../views/Home'
 import NavBar from './NavBar'
+import Banner from './Banner'
 
 function App () {
     return(
         <div>
             <NavBar />
+            <Banner />
             <Home />
         </div>
     )
